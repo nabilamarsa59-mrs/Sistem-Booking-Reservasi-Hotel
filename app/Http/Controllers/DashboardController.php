c466538692930fb731de43eceba6a4771f27adb6
Refandyy <computerb32@gmail.com>
2026-03-17 19:36:01 -0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,1 +1,9 @@
+<?php
+namespace app/Http/Controllers;
+use Illuminate/Http/Request;
 
+class DashboardController exteneds Controller {
+    public function index() {
+        return view('dashboard');
+    }
+}
