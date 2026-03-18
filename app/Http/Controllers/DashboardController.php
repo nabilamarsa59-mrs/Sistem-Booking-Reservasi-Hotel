b39fc36fd26c24a0e31c88ca1268270c9acdbdab
Refandyy <computerb32@gmail.com>
2026-03-17 19:51:15 -0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,6 +1,6 @@
 <?php
-namespace app/Http/Controllers;
-use Illuminate/Http/Request;
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller {
     public function index() {
